2be1a0250715200e29676f6fbd3c0fe5e283bce7
yysd94 <yupianon@gmail.com>
2024-05-23 23:30:36 +0900

    placeable?メソッドが置けない判定の場合にfalseの値を返すように修正しました。

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -87,6 +87,7 @@ module ReversiMethods
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
+    false
   end
 
   def count_stone(board, stone_color)
